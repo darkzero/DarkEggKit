3a3a9e18d3409ff0bfb90737d1508bfe2689cbd4
darkzero <darkzero_mk2@hotmail.com>
2019-10-01 20:29:27 +0900

    update for 0.3.1
    * add about ViewController to sample app (not used in this version)
    * delele some unused comment
    * update podspec to 0.3.1
    * etc.

---
+++ Example/DarkEggKit/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  DarkEggKit_Example
+//
+//  Created by Yuhua Hu on 2019/09/30.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension AboutViewController {
+    private func loadAboutText() {
+        
+    }
+}
